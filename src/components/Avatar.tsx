88b870a6ff0211b673bfec8d4fd63d029c8cd609
João Dias <joaoeduardodias123@gmail.com>
2022-11-08 10:41:47 -0300

    feat: finished component Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,12 +5,12 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean
 }
 
-export function Avatar({ src, hasBorder = true,alt }:AvatarProps) {
+export function Avatar({hasBorder = true, ...props}:AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+     {...props}
+     
     />
   );
 }
